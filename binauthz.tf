ffb245511ce52d0e36c079ceb50afee2ec2d4fce
Ivan Gatnau Lopez <ilgatnau@gmail.com>
2022-08-22 08:54:39 +0100

    initial commit

---
+++ binauthz.tf
@@ -0,0 +1,29 @@
+data "google_kms_crypto_key_version" "attestor-key-version" {
+  crypto_key = "projects/vm-import-346415/locations/global/keyRings/keyring-example/cryptoKeys/asymmetric-key-example"
+}
+
+resource "google_binary_authorization_attestor" "attestor" {
+  project = "vm-import-346415"
+  name    = "demo-attestor"
+  attestation_authority_note {
+    note_reference = google_container_analysis_note.note.name
+    public_keys {
+      id = data.google_kms_crypto_key_version.attestor-key-version.id
+      pkix_public_key {
+        public_key_pem      = data.google_kms_crypto_key_version.attestor-key-version.public_key[0].pem
+        signature_algorithm = data.google_kms_crypto_key_version.attestor-key-version.public_key[0].algorithm
+      }
+    }
+  }
+}
+
+#### Vulnerability Analysis Note
+resource "google_container_analysis_note" "note" {
+  project = "vm-import-346415"
+  name    = "test-attestor-note"
+  attestation_authority {
+    hint {
+      human_readable_name = "Attestor Note"
+    }
+  }
+}
