091f2f5c4c4ce799ffdb9a9f5f01507d57756074
Ivan Gatnau Lopez <ilgatnau@gmail.com>
2022-08-23 09:06:56 +0100

    some cleanup

--- iam.tf
+++ iam.tf
@@ -4,25 +4,6 @@ resource "google_service_account" "attestor_service_account" {
   project = google_project.attestor.project_id
 }
 
-
-# resource "google_iam_policy" "attestor_service_account" {
-#   service_account_id = google_service_account.attestor_service_account.id
-
-#   for_each = toset(
-#     [
-#       "roles/binaryauthorization.attestorsViewer",
-#       "roles/cloudkms.signerVerifier",
-#       "roles/containeranalysis.notes.attacher",
-#       "roles/containeranalysis.occurrences.editor", 
-#     ]
-#   )
-
-#   role     = each.key
-#   members   = [
-#     "serviceAccount:binauthz-sa@binauthz-attestor-test-project.iam.gserviceaccount.com"
-#   ]
-# }
-
 resource "google_project_iam_member" "sa_attestor" {
   project = google_project.attestor.project_id
 
